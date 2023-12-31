ad81070efce6157334efd6d54a34bb7f9bff04dd
Md. Asif Rahman <eng.asifrahman@gmail.com>
2024-01-01 01:01:03 +0600

    Controller edited

--- src/app/Http/Controllers/TestController.php
+++ src/app/Http/Controllers/TestController.php
@@ -9,5 +9,23 @@ class TestController extends Controller
     //
 
 
-    
+    public function A() {
+        // Implement method A here
+    }
+
+    public function B() {
+        // Implement method B here
+    }
+
+    public function C() {
+        // Implement method C here
+    }
+
+    private function C_Helper() {
+        // Implement helper method of method C here
+    }
+
+    public function D() {
+        // Implement method D here
+    }
 }
